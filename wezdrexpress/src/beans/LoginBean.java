a1589702cff865c3e6080ccdfef72b08b6b2d5c3
Edison zamora <37243420+edisonzamora@users.noreply.github.com>
2019-03-11 10:34:44 +0100

    [recambios]

--- wezdrexpress/src/beans/LoginBean.java
+++ wezdrexpress/src/beans/LoginBean.java
@@ -3,15 +3,14 @@ package beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name="loginBean")
+@ManagedBean(name="loginBean" , eager=true)
 @SessionScoped
 public class LoginBean {
 
-	public LoginBean(){
-		
-	}
 	private String correo;
+	
 	private String password;
+	
 	public String getCorreo() {
 		return correo;
 	}
--- wezdrexpress/src/beans/LoginContollerbean.java
+++ wezdrexpress/src/beans/LoginContollerbean.java
@@ -4,22 +4,31 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name="LoginContollerbean")
+@ManagedBean(name="loginContoller")
 @SessionScoped
 public class LoginContollerbean {
+	
 	@ManagedProperty(value="#{loginBean}")
 	private LoginBean login;
 
-	public String loginController(){
+	public String aceptar(){
 		if(login!=null){
 			
 			if(!login.getCorreo().equals("") ||!login.getPassword().equals("")){
 				
-				return "homeView";
+				return "homeView?faces-redirect=true";
 			}
 		}
 		
 		return "loginView";
 		
+	}
+
+	public LoginBean getLogin() {
+		return login;
+	}
+
+	public void setLogin(LoginBean login) {
+		this.login = login;
 	}
 }
