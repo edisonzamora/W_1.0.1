b714558d427ee667d36dcc505c5a634b690c6c21
Edison zamora <37243420+edisonzamora@users.noreply.github.com>
2019-03-22 13:54:48 +0100

    [ezd]-seguridad login

--- wezdrexpress/src/com/rexseguridad/SegurityRexpress.java
+++ wezdrexpress/src/com/rexseguridad/SegurityRexpress.java
@@ -1,10 +1,12 @@
 package com.rexseguridad;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -13,7 +15,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
+
 public class SegurityRexpress implements Filter {
+private  static Logger LOG=Logger.getLogger(SegurityRexpress.class);
 	private ServletContext context;
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -28,13 +33,27 @@ public class SegurityRexpress implements Filter {
 			throws IOException, ServletException {
 		HttpServletRequest req=(HttpServletRequest)request;
 		HttpServletResponse res = (HttpServletResponse) response;
-		
+	String tipo=	(String) req.getSession().getAttribute("usuario");
+		if(tipo!=null) {
+			 res.sendRedirect("/wezdrexpress/faces/view/homeView.xhtml");
+		}else {
 		String uri = req.getRequestURI();
-		this.context.log("Requested Resource::"+uri);
-		
-		HttpSession session = req.getSession(false);
-		
-		chain.doFilter(request, response);
+		 LOG.debug("Requested Resource::URI:: " +uri);
+		Enumeration<String> headers=req.getHeaderNames();
+		 while (headers.hasMoreElements()) {
+		      String key = headers.nextElement();
+		     LOG.debug( key + "<-- " + req.getHeader(key ));
+		    }
+		 RequestDispatcher rsdpc=null;
+//		 rsdpc = req.getRequestDispatcher("/wezdrexpress/faces/view/loginView.xhtml");  
+//		 rsdpc.include(req, res);  
+		 res.sendRedirect("/wezdrexpress/faces/view/loginView.xhtml");
+
+//		
+//		HttpSession session = req.getSession(false);
+//		
+//		chain.doFilter(request, response);
+		}
 	}
 
 	@Override
